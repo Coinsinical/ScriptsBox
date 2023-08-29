85df2dd3339858fe61eba77f1afa4c3074449fcf
Coinsinical <88478301+Coinsinical@users.noreply.github.com>
2023-08-29 23:09:55 +0800

    convert file to Unix format

--- safety_enhace.sh
+++ safety_enhace.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Telegram Bot API密钥和聊天ID（请替换为你自己的）
-telegram_api_key=$1
-chat_id=$2
-
 # 生成随机端口号
 new_port=$[$RANDOM%16384+49152]
 
@@ -11,13 +7,16 @@ new_port=$[$RANDOM%16384+49152]
 sed -i "s/^#Port.*/Port $new_port/" /etc/ssh/sshd_config
 
 # 重启SSH服务
-service sshd restart
+service ssh restart
 
 # 获取本机IP地址
 ip_address=$(curl -s ip.sb -4)
 
-
+# Telegram Bot API密钥和聊天ID（请替换为你自己的）
+telegram_api_key="5645065115:AAFYK-uaclJIgEpklDjXC5gca24UJvQUkUk"
+chat_id="1025891105"
 
 # 发送通知到Telegram
 message="SSH端口已修改为$new_port，当前的IP地址是：$ip_address"
+echo $message
 curl -s -X POST "https://api.telegram.org/bot${telegram_api_key}/sendMessage" -d "chat_id=${chat_id}&text=${message}"
